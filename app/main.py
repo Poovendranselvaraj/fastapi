0db641e6068e672d1c5a0a4bd2a7b266351d9203
poove <poovendranselavaraj333@gmail.com>
2024-08-29 19:23:38 +0530

    5.28.03

--- app/main.py
+++ app/main.py
@@ -50,57 +50,74 @@ def root():
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
 
-    post=db.query(models.Post).all()
-    return{"status": "success"}
-
+    posts=db.query(models.Post).all()
+    print(posts)
+    return{"data": "successfull"}
+ 
 @app.get("/posts")
-def get_posts():
-    print("jjjhh")
-    cursor.execute("""SELECT * FROM posts""")
-    posts=cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts=cursor.fetchall()
+    posts=db.query(models.Post).all()
     return {"data": posts} 
 
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts(title, content, published) VALUES(%s,%s,%s) RETURNING * """, 
-                   (post.title, post.content, post.published))
-    new_post=cursor.fetchone()
+def create_posts(post: Post,db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts(title, content, published) VALUES(%s,%s,%s) RETURNING * """, 
+    #                (post.title, post.content, post.published))
+    # new_post=cursor.fetchone()
 
-    conn.commit()
-   
-    return {"data": new_post}
+    # conn.commit()
+   new_post=models.Post(**post.model_dump())
+   db.add(new_post)
+   db.commit()
+   db.refresh(new_post)
+
+   return {"data": new_post}
   
 @app.get("/posts/{id}") 
-def get_post(id:str):
-   cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id)))
-   post=cursor.fetchone() 
-   if not post:
+def get_post(id:int,db: Session = Depends(get_db)):
+#    cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id)))
+#    post=cursor.fetchone() 
+     post=db.query(models.Post).filter(models.Post.id == id).first()
+
+     if not post:
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-   return{"post_detail":post} 
+     return{"post_detail":post} 
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
+def delete_post(id:int,db: Session = Depends(get_db)):
     
-    cursor.execute("""DELETE FROM posts WHERE id=%s returning *""", (str(id),))
-    deleted_post=cursor.fetchone()
-    print("deleted successfully")
-    conn.commit()
+    # cursor.execute("""DELETE FROM posts WHERE id=%s returning *""", (str(id),))
+    # deleted_post=cursor.fetchone()
+    # conn.commit()
+    post=db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
  
 @app.put("/posts/{id}")
-def update_post(id:int, post: Post):
+def update_post(id:int, updated_post: Post,db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,(post.title, post.content, post.published,str(id)))
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,(post.title, post.content, post.published,str(id)))
 
-    updated_post=cursor.fetchone()
-    conn.commit()
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+
+    post_query=db.query(models.Post).filter(models.Post.id == id)
+
+    post=post_query.first()
     
-    if updated_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post_query.update(updated_post.model_dump(),synchronize_session=False)
 
-    return {"data": updated_post}
+    db.commit()
+
+    return {"data": post_query.first()}
