71b663ecc9f31c0c98ef4a1d996157636bc6dc93
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-14 14:24:24 +0530

    16.31

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.main import app
-from app import schemas
 from app.config import settings
 from app.database import get_db
 from app.database import Base
+from app.oauth2 import create_access_token
 from alembic import command
 
 # SQLALCHEMY_DATABASE_URL='postgresql://postgres:poovendran@localhost:5432/fastapi_test'
@@ -45,7 +45,17 @@ def test_user(client):
     res=client.post("/users/",json=user_data)
     
     assert res.status_code==201
-    print(res.json())
     new_user=res.json()
     new_user["password"]=user_data["password"]
     return new_user  
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id":test_user["id"]})
+
+def authorized_client(client,token):
+    client.headers={
+        **client.headers,
+        "Authorization":f"Bearer {token}"
+    }
+    return client
